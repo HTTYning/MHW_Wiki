b5b9dbe3b94934496e8da69eb92e020e7c13df41
noteBook-Murasame <HTTYning@163.com>
2024-04-01 14:03:18 +0800

    完成武器页面基本模板，远程武器未完善

--- pages/weapons/weapons.vue
+++ pages/weapons/weapons.vue
@@ -1,9 +1,58 @@
 <template>
 	<view>
 		<view class="weaponList">
-			<view>一共 {{weapons.length}} 把太刀</view>
+			<view class="weapon-number">{{weaponsType}} / {{weapons.length}}</view>
 			<view class="weapon-item" v-for="item in weapons" :key="item.id">
-				<view>{{item.id}} - {{item.name}}</view>
+				<view class="weapon-titile-bar">
+					<view class="weapon-icon">
+						<img :src="item.assets.icon" alt="" mode="aspectFit">
+					</view>
+					<view class="weapon-name">{{item.name}}</view>
+				</view>
+				<view class="weapon-attribute">
+					<view class="weapon-img">
+						<img :src="item.assets.image" alt="" mode="aspectFit">
+					</view>
+					<view class="weapon-attribute-value">
+						<view class="weapon-rarity">
+							rarity: {{item.rarity}}
+						</view>
+						<view class="weapon-attack">
+							attack: {{item.attack.display}}({{item.attack.raw}})
+						</view>
+						<view class="weapon-affinity">
+							affinity: {{(item.attributes.affinity?item.attributes.affinity:'0')+'%'}}
+						</view>
+						<view class="weapon-elements" v-if="item.elements" v-for="elements in item.elements" :key="item.index">
+							elements: {{elements.type}} {{elements.damage}}
+							<br>
+							{{item.elderseal ? "elderseal: "+item.elderseal:""}}
+						</view>
+						<view class="weapon-slots">
+							<image :src="'../../static/weapon/slot-'+(item.slots[0].rank ? item.slots[0].rank : slotsNullStr)+'.png'" mode="aspectFit"></image>
+							<image :src="'../../static/weapon/slot-'+(item.slots[1].rank ? item.slots[1].rank : slotsNullStr)+'.png'" mode="aspectFit"></image>
+							<image :src="'../../static/weapon/slot-'+(item.slots[2].rank ? item.slots[2].rank : slotsNullStr)+'.png'" mode="aspectFit"></image>
+						</view>
+						<view class="weapon-durability" v-if="item.durability">
+							<view :style="{backgroundColor: 'red', width:((item.durability[0].red/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'orange', width:((item.durability[0].orange/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'yellow', width:((item.durability[0].yellow/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'green', width:((item.durability[0].green/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'blue', width:((item.durability[0].blue/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'white', width:((item.durability[0].white/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'purple', width:((item.durability[0].purple/400)*100)+'%'}"></view>
+						</view>
+						<!-- <view class="weapon-durability" v-for="(dur,durIndex) in item.durability" :key="durIndex">
+							<view :style="{backgroundColor: 'red', width:((dur.red/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'orange', width:((dur.orange/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'yellow', width:((dur.yellow/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'green', width:((dur.green/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'blue', width:((dur.blue/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'white', width:((dur.white/400)*100)+'%'}"></view>
+							<view :style="{backgroundColor: 'purple', width:((dur.purple/400)*100)+'%'}"></view>
+						</view> -->
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -14,11 +63,14 @@
 		data() {
 			return {
 				weapons: [],
+				slotsNullStr:"null",
 				weaponsType:''
 			};
 		},
 		onLoad() {
-			this.weaponsType = 'long-sword';
+			// this.weaponsType = 'long-sword';0
+			this.weaponsType = 'great-sword';
+			// this.weaponsType = 'light-bowgun';
 			// this.removeData('weaponsData');
 			this.checkData()
 		},
@@ -47,6 +99,7 @@
 					const value = uni.getStorageSync('weaponsData');
 					if (value) {
 						console.log('武器本地数据存在!');
+						console.log(value);
 						this.renderingData();// 渲染数据
 						return true;
 					} else {
@@ -92,5 +145,80 @@
 </script>
 
 <style lang="scss">
-	.weaponList {}
+	.weapon-number{
+		padding: 5px;
+		font-size: 22px;
+		font-weight: bold;
+		background-color: rgba(0, 0, 0, .2);
+	}
+	.weaponList {
+		.weapon-item{
+			width: 100%;
+			display: flex;
+			flex-direction: column;
+			box-sizing: border-box;
+			margin-bottom: 5px;
+			border-top: 2px solid gray;
+			.weapon-titile-bar{
+				height: 30px;
+				display: flex;
+				flex-direction: row;
+				justify-content: left;
+				align-items: center;
+				padding: 5px;
+				border-bottom: 2px solid rgba(0, 0, 0, .1);
+				.weapon-icon{
+					width: 30px;
+					height: 30px;
+					image{
+						width: 100%;
+						height: 100%;
+					}
+				}
+				.weapon-name{
+					margin-left: 10px;
+					size: 20px;
+					font-weight: bold;
+					color:black;
+				}
+			}
+			.weapon-attribute{
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+				padding: 5px 0;
+				view{
+					margin: 3px 0;
+				}
+				.weapon-img{
+					width: 100px;
+					height: 100px;
+					image{
+						width: 100%;
+						height: 100%;
+					}
+				}
+				.weapon-slots{
+					display: flex;
+					flex-direction: row;
+					image{
+						width: 20px;
+						height: 20px;
+					}
+				}
+				.weapon-durability{
+					width: 150px;
+					height: 10px;
+					display: flex;
+					flex-direction: row;
+					align-items: center;
+					background-color: gray;
+					border: 1px solid black;
+					view{
+						height: 100%;
+					}
+				}
+			}
+		}
+	}
 </style>
--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -1543,7 +1543,7 @@ function initData(vueOptions, context) {
     try {
       data = data.call(context); // 支持 Vue.prototype 上挂的数据
     } catch (e) {
-      if (Object({"VUE_APP_DARK_MODE":"false","VUE_APP_NAME":"MHW_Wiki","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"NODE_ENV":"development","VUE_APP_DARK_MODE":"false","VUE_APP_NAME":"MHW_Wiki","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.warn('根据 Vue 的 data 函数初始化小程序 data 失败，请尽量确保 data 函数中不访问 vm 对象，否则可能影响首次数据渲染速度。', data);
       }
     }
@@ -8897,7 +8897,7 @@ function type(obj) {
 
 function flushCallbacks$1(vm) {
     if (vm.__next_tick_callbacks && vm.__next_tick_callbacks.length) {
-        if (Object({"VUE_APP_DARK_MODE":"false","VUE_APP_NAME":"MHW_Wiki","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+        if (Object({"NODE_ENV":"development","VUE_APP_DARK_MODE":"false","VUE_APP_NAME":"MHW_Wiki","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:flushCallbacks[' + vm.__next_tick_callbacks.length + ']');
@@ -8918,14 +8918,14 @@ function nextTick$1(vm, cb) {
     //1.nextTick 之前 已 setData 且 setData 还未回调完成
     //2.nextTick 之前存在 render watcher
     if (!vm.__next_tick_pending && !hasRenderWatcher(vm)) {
-        if(Object({"VUE_APP_DARK_MODE":"false","VUE_APP_NAME":"MHW_Wiki","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"NODE_ENV":"development","VUE_APP_DARK_MODE":"false","VUE_APP_NAME":"MHW_Wiki","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:nextVueTick');
         }
         return nextTick(cb, vm)
     }else{
-        if(Object({"VUE_APP_DARK_MODE":"false","VUE_APP_NAME":"MHW_Wiki","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"NODE_ENV":"development","VUE_APP_DARK_MODE":"false","VUE_APP_NAME":"MHW_Wiki","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance$1 = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance$1.is || mpInstance$1.route) + '][' + vm._uid +
                 ']:nextMPTick');
@@ -9021,7 +9021,7 @@ var patch = function(oldVnode, vnode) {
     });
     var diffData = this.$shouldDiffData === false ? data : diff(data, mpData);
     if (Object.keys(diffData).length) {
-      if (Object({"VUE_APP_DARK_MODE":"false","VUE_APP_NAME":"MHW_Wiki","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"NODE_ENV":"development","VUE_APP_DARK_MODE":"false","VUE_APP_NAME":"MHW_Wiki","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + this._uid +
           ']差量更新',
           JSON.stringify(diffData));
@@ -9590,17 +9590,7 @@ function normalizeComponent (
 /* 36 */,
 /* 37 */,
 /* 38 */,
-/* 39 */,
-/* 40 */,
-/* 41 */,
-/* 42 */,
-/* 43 */,
-/* 44 */,
-/* 45 */,
-/* 46 */,
-/* 47 */,
-/* 48 */,
-/* 49 */
+/* 39 */
 /*!************************************************************************************************!*\
   !*** ./node_modules/@dcloudio/vue-cli-plugin-uni/packages/@babel/runtime/regenerator/index.js ***!
   \************************************************************************************************/
@@ -9609,11 +9599,11 @@ function normalizeComponent (
 
 // TODO(Babel 8): Remove this file.
 
-var runtime = __webpack_require__(/*! @babel/runtime/helpers/regeneratorRuntime */ 50)();
+var runtime = __webpack_require__(/*! @babel/runtime/helpers/regeneratorRuntime */ 40)();
 module.exports = runtime;
 
 /***/ }),
-/* 50 */
+/* 40 */
 /*!*******************************************************************!*\
   !*** ./node_modules/@babel/runtime/helpers/regeneratorRuntime.js ***!
   \*******************************************************************/
@@ -9934,7 +9924,7 @@ function _regeneratorRuntime() {
 module.exports = _regeneratorRuntime, module.exports.__esModule = true, module.exports["default"] = module.exports;
 
 /***/ }),
-/* 51 */
+/* 41 */
 /*!*****************************************************************!*\
   !*** ./node_modules/@babel/runtime/helpers/asyncToGenerator.js ***!
   \*****************************************************************/
